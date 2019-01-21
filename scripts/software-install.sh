e08300c56b920af8cc6aedd06a8ce4e5a8bad486
Bastien Philippe <bastien.philippe@soyhuce.fr>
2019-01-21 18:47:21 +0100

    ajout -y à l'install docker

--- scripts/software-install.sh
+++ scripts/software-install.sh
@@ -42,7 +42,7 @@ sudo apt install -y nodejs
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 sudo systemctl disable docker
 sudo adduser ${USER} docker
 
