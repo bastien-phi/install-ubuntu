c1f6d275c97afd6cf64e91c3eaeac222f508129d
Bastien Philippe <bastien.philippe@soyhuce.fr>
2021-09-07 17:14:05 +0200

    Fix brave install

--- scripts/software-install.sh
+++ scripts/software-install.sh
@@ -130,7 +130,7 @@ if [[ ! -x "$(which brave-browser)" ]]; then
   echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | \
     sudo tee /etc/apt/sources.list.d/brave-browser-release.list
   sudo apt update
-  sudo apt install brave-browser
+  sudo apt install -y brave-browser
 fi
 
 # Installation glab (https://github.com/profclems/glab#installation)
